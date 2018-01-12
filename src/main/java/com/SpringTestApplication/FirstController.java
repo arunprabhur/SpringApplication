d89c7a2b17ef2cf8098fe897135460f493bdf821
Arun Prabhu <prabhu.arun10@gmail.com>
2018-01-13 01:19:03 +0530

    Updated Code

--- src/main/java/com/SpringTestApplication/FirstController.java
+++ src/main/java/com/SpringTestApplication/FirstController.java
@@ -1,13 +1,27 @@
 package com.SpringTestApplication;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class FirstController {
-@RequestMapping("/hello")
+@RequestMapping(value="/hello")
 	public String hello()
 	{
 		return "Welcome User";
 	}
+@RequestMapping("/")  
+public String index(){  
+    return "index";  
+}  
+@RequestMapping(value="/save", method=RequestMethod.POST)  
+public ModelAndView save(@ModelAttribute User user){  
+    ModelAndView modelAndView = new ModelAndView();  
+modelAndView.setViewName("user-data");      
+modelAndView.addObject("user", user);    
+return modelAndView;  
+}  
 }
